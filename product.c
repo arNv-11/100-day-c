f79638db8da7a9b5c2a1168a3227ce05b60a7b0a
Arnav <arnavpranjal22@gmail.com>
2026-08-23 18:58:50 +0530

    Update product.c

--- product.c
+++ product.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main()
+{
+    long long product = 1;
+    int i;
+
+    for (i = 2; i <= 100; i += 2)
+    {
+        product = product * i;
+    }
+
+    printf("Product of even numbers from 1 to 100 = %lld", product);
+
+    return 0;
+}

e48ed95dd9078c95a2ccdccfb010f254deed8f34
Arnav <arnavpranjal22@gmail.com>
2026-08-24 20:51:37 +0530

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main()
+{
+    int n, reverse = 0, reaminder;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n != 0)
+    {
+        reaminder = n % 10;
+        reverse = reverse * 10 + reaminder;
+        n /= 10;
+    }
+
+    printf("Recersed = %d\n", reverse);
+
+    return 0;
+}

cc587e844d604febdc151389c7bf1e04974ddd93
Arnav <arnavpranjal22@gmail.com>
2026-08-27 19:16:14 +0530

    Update HCF.c

--- HCF.c
+++ HCF.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main()
+{
+    int a, b, i, HCF;
+    printf("Enter a two numbers: ");
+    scanf("%d" "%d", &a, &b);
+
+    for (i = 1; i <= a && i <= b; i++)
+    {
+        if (a % i == 0 && b % i == 0)
+        {
+            HCF = i;
+        }
+    }
+
+    printf("HCF of %d and %d is %d\n", a, b, HCF);
+
+    return 0;
+}

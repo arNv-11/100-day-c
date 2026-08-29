3d735b0592dd5e2f32a6611f6fe1b563fce988ce
Arnav <arnavpranjal22@gmail.com>
2026-08-29 10:26:18 +0530

    Update Compliment.c

--- Compliment.c
+++ Compliment.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main()
+{
+    long long binary, digit, complement = 0, place = 1;
+
+    printf("Enter a binary number: ");
+    scanf("%lld", &binary);
+
+    while (binary > 0)
+    {
+        digit = binary % 10;
+
+        if (digit == 0)
+            digit = 1;
+        else
+            digit = 0;
+
+        complement = complement + digit * place;
+        place = place * 10;
+
+        binary = binary / 10;
+    }
+
+    printf("1's Complement = %lld", complement);
+
+    return 0;
+}

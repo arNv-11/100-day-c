f2257a63d49bcfd32e80c259ed1aef1d1ad70c39
Arnav <arnavpranjal22@gmail.com>
2026-08-31 20:17:32 +0530

    Implement Strong Number check in C

--- strong number.c
+++ strong number.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int main()
+{
+    int num, original, digit;
+    int sum = 0, fact;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while (num > 0)
+    {
+        digit = num % 10;
+
+        fact = 1;
+        for (int i = 1; i <= digit; i++)
+        {
+            fact = fact * i;
+        }
+
+        sum = sum + fact;
+        num = num / 10;
+    }
+
+    if (sum == original)
+        printf("%d is a Strong Number", original);
+    else
+        printf("%d is not a Strong Number", original);
+
+    return 0;
+}

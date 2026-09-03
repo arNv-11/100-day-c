5948ba3e03e98e356c28c7a2ea29485752c22d24
Arnav <arnavpranjal22@gmail.com>
2026-09-03 17:55:37 +0530

    Update pattern3.c

--- pattern3.c
+++ pattern3.c
@@ -1,1 +1,24 @@
+```c
+#include <stdio.h>
 
+int main()
+{
+    int i, j;
+
+    for (i = 5; i >= 1; i--)
+    {
+        for (j = 5; j > i; j--)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+```

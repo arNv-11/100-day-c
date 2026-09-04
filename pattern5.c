a2b7c4f7fee4d678f24bfc0b40f13c71561dc7a5
Arnav <arnavpranjal22@gmail.com>
2026-09-04 22:47:10 +0530

    Update pattern5.c

--- pattern5.c
+++ pattern5.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int main()
+{
+    int i, j;
+
+    for (j = 1; j <= 1; j++)
+        printf("*");
+    printf("\n\n");
+
+    for (j = 1; j <= 4; j++)
+        printf("*");
+    printf("\n\n");
+
+    for (j = 1; j <= 5; j++)
+        printf("*");
+    printf("\n\n");
+
+    for (j = 1; j <= 3; j++)
+        printf("*");
+    printf("\n\n");
+
+    for (j = 1; j <= 1; j++)
+        printf("*");
+
+    return 0;
+}

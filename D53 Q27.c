c6566e219b7d2ab8ed9d9a8b8f7d8d33c9729ba7
Arnav <arnavpranjal22@gmail.com>
2026-09-06 18:20:53 +0530

    Update D53 Q27.c

--- D53 Q27.c
+++ D53 Q27.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main()
+{
+    int i, j, n = 5;
+
+    // Upper half
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // Lower half
+    for (i = n - 1; i >= 1; i--)
+    {
+        for (j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

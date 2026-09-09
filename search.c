64983360833bcd31eb76b2e20e52613d1012f317
Arnav <arnavpranjal22@gmail.com>
2026-09-09 18:58:59 +0530

    Update search.c

--- search.c
+++ search.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+int main()
+{
+    int arr[100], n, key, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+        {
+            printf("%d found at position %d", key, i + 1);
+            break;
+        }
+    }
+
+    if (i == n)
+    {
+        printf("%d not found in the array", key);
+    }
+
+    return 0;
+}

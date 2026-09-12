1da07e18be8dbda69e3320b1fdef561396040fa8
Arnav <arnavpranjal22@gmail.com>
2026-09-12 12:33:03 +0530

    Implement element insertion in an array
    This program allows the user to insert an element into a specified position in an array and displays the updated array.

--- insert an element in an array.c
+++ insert an element in an array.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
 
+int main()
+{
+    int a[100], n, element, position, i;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements:\n");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter the element to insert: ");
+    scanf("%d", &element);
+
+    printf("Enter the position: ");
+    scanf("%d", &position);
+
+    for (i = n; i >= position; i--)
+    {
+        a[i] = a[i - 1];
+    }
+
+    a[position - 1] = element;
+
+    n++;
+
+    printf("Array after insertion:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}

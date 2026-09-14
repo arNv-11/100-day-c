4d0e22af38c99592b14bd623e9631589e5127e58
Arnav <arnavpranjal22@gmail.com>
2026-09-14 10:11:10 +0530

    Implement matrix input and display functionality
    Add a program to input and display a matrix.

--- sum of elements.c
+++ sum of elements.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+int main()
+{
+    int matrix[10][10];
+    int rows, columns, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &columns);
+
+    printf("Enter the elements of the matrix:\n");
+
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < columns; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("The matrix is:\n");
+
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < columns; j++)
+        {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

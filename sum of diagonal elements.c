29a104ff25cd0e248b8e3370e8b75ff0f67bca09
Arnav <arnavpranjal22@gmail.com>
2026-09-17 20:36:26 +0530

    Add program to sum diagonal elements of a matrix
    This program calculates the sum of the main diagonal elements of a square matrix entered by the user.

---
+++ sum of diagonal elements.c
@@ -0,0 +1,29 @@
+// Sum of diagonal elements
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10], n, i, j, sum = 0;
+
+    printf("Enter the size of matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter the matrix elements:\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Find sum of main diagonal
+    for (i = 0; i < n; i++)
+    {
+        sum = sum + a[i][i];
+    }
+
+    printf("Sum of main diagonal elements = %d", sum);
+
+    return 0;
+}

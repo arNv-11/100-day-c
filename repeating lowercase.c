b5d44ddcaf4e243682a8aca157f07631e0645b70
Arnav <arnavpranjal22@gmail.com>
2026-09-24 16:35:41 +0530

    Add program to find first repeating lowercase letter
    This program checks for the first repeating lowercase alphabet in a given string and prints it.

---
+++ repeating lowercase.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, count[26] = {0};
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            count[str[i] - 'a']++;
+
+            if (count[str[i] - 'a'] == 2)
+            {
+                printf("First repeating lowercase alphabet: %c", str[i]);
+                return 0;
+            }
+        }
+    }
+
+    printf("No repeating lowercase alphabet found");
+
+    return 0;
+}
